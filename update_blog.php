d3c7c7a03070066f4f43810eb961b881564f07ca
lies046 <skytree.skysky@gmail.com>
2020-07-18 15:51:01 +0900

    Add Update

--- update_blog.php
+++ update_blog.php
@@ -5,11 +5,17 @@
   require_once 'functions/Security.php';
   $s = new Security();
   $id = $s->f($_GET['id']);
+  $title = $s->f($_POST['title']);
+  $category = $s->f($_POST['category']);
+  $message = $s->f($_POST['message']);
 
   $db = Db();
-  $stt = $db->prepare('DELETE FROM post WHERE id=:id');
+  $stt = $db->prepare('UPDATE post SET title=:title, category=:category, post=:message WHERE id=:id');
   $stt->bindvalue(':id', $id);
+  $stt->bindvalue(':title', $title);
+  $stt->bindvalue(':category', $category);
+  $stt->bindvalue(':message', $message);
   $stt->execute();
 
-  $_SESSION['success'] = $id . '番の記事を削除しました。';
-  header('Location: index.php');
+  $_SESSION['success'] = $id . '番の記事を更新しました。';
+  header('Location: update.php');
