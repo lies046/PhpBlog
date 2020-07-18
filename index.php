5c1b5ed11a55005306fb2da35079329f34e8c2c6
lies046 <skytree.skysky@gmail.com>
2020-07-18 10:41:47 +0900

    init commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+  <html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  </head>
+  <body>
+    <div id="sidebar">
+      <ul>
+        <li><a href="#">記事投稿</a></li>
+        <li><a href="#">記事一覧</a></li>
+        <li><a href="#">カテゴリー管理</a></li>
+        <li><a href="#">記事管理</a></li>
+        <li><a href="#">その他</a></li>
+      </ul>
+    </div>
+  </body>
+</html>
