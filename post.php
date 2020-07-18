30dfceca9e65e64ee3c61565e47596bb0f0839bc
lies046 <skytree.skysky@gmail.com>
2020-07-18 11:31:44 +0900

    Modified Post.php

--- post.php
+++ post.php
@@ -6,4 +6,19 @@
   $message = $_POST['message'];
 
   //DBに保存　DBに接続して、データを挿入
-  
+  try{
+  $dns = 'mysql:host=localhost;dbname=blog;charset=utf8';
+  $user = 'root';
+  $pass = '';
+
+  $db = new PDO($dns, $user, $pass);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }catch(PDOException $e){
+    print $e;
+  }
+
+  $stt = $db->prepare('INSERT INTO post(title, category, post) VALUE(:title, :category, :message)');
+  $stt->bindValue(':title', $title);
+  $stt->bindValue(':category', $category);
+  $stt->bindValue(':message', $message);
+  $stt->execute();
