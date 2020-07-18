5eb4c83fdc5715e03194bb9bc1320f6435c783ef
lies046 <skytree.skysky@gmail.com>
2020-07-18 10:56:01 +0900

    Add Bootstrap and Css

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body>
     <div id="sidebar">
@@ -15,6 +16,12 @@
         <li><a href="#">記事管理</a></li>
         <li><a href="#">その他</a></li>
       </ul>
+    </div>
+
+    <div id="main">
+      test
+    </div>
+    <div class="clear">
     </div>
   </body>
 </html>
